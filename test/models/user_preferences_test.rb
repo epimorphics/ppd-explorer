99d4169b0d4c24a0c5ebf73c0b1d533b255ffc29
Ian Dickinson <i.j.dickinson@gmail.com>
2014-03-25 17:53:01 +0000

    Updated tests for user preferences

---
+++ test/models/user_preferences_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+describe "UserPreferences" do
+
+  it "should provide access to the query parameters" do
+    prefs = UserPreferences.new( {"street" => "bar"} )
+    prefs.param("street").must_equal "bar"
+    prefs.param("town").must_be_nil
+  end
+
+  it "should santize unrecognised inputs" do
+    prefs = UserPreferences.new( {"fribble" => "bar"} )
+    prefs.param("fribble").must_be_nil
+  end
+
+  it "should report a parameter as present, or not" do
+    prefs = UserPreferences.new( {"street" => "bar"} )
+    assert( prefs.present?( :street ) )
+    assert( !prefs.present?( :town ) )
+  end
+
+  it "should report a parameter as present with a given value, or not" do
+    prefs = UserPreferences.new( {"street" => "bar"} )
+    assert( prefs.present?( :street, "bar" ) )
+    assert( !prefs.present?( :street, "blam" ) )
+  end
+
+  it "should report a parameter as present, with a list of values, or not" do
+    prefs = UserPreferences.new( {"nb" => ["true", "false"]} )
+    assert( prefs.present?( :nb, "true" ) )
+    assert( prefs.present?( :nb, "false" ) )
+    assert( !prefs.present?( :nb, "flimflam" ) )
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,22 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/spec'
+require 'capybara/rails'
+require 'minitest/capybara'
+require 'json_expressions/minitest'
+require 'pry'
 
-class ActiveSupport::TestCase
-  # Add more helper methods to be used by all tests here...
+class AcceptanceTest < Minitest::Unit::TestCase
+  include Capybara::DSL
+  include Minitest::Capybara::Assertions
+
+  def teardown
+    Capybara.reset_session!
+    Capybara.use_default_driver
+  end
+end
+
+class AcceptanceSpec < AcceptanceTest
+  extend Minitest::Spec::DSL
 end
