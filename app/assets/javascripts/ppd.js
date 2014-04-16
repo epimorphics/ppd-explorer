1c7a06bc5c99f562926cee1babcd62f3a808b081
Ian Dickinson <i.j.dickinson@gmail.com>
2014-04-16 19:50:30 +0100

    Changing months or years sets default day of month. Also display date in more readable form. Issue #20.

--- app/assets/javascripts/ppd.js
+++ app/assets/javascripts/ppd.js
@@ -7,14 +7,36 @@ var Ppd = function() {
   var initControls = function() {
     console.log( "Setting datepicker on " + $(".date-picker").length);
     $(".date-picker").datepicker( {
-      dateFormat: "yy-mm-dd",
+      dateFormat: "d MM yy",
       changeMonth: true,
-      changeYear: true
+      changeYear: true,
+      onChangeMonthYear: onChangeMonthYear
     } );
   };
 
   var bindEvents = function() {
 
+  };
+
+  var onChangeMonthYear = function( year, month, dp ) {
+    var control_name = $(this).attr( "name" );
+    var defaultDate = defaultDayOfMonth( year, month, control_name );
+    $(this).datepicker( "setDate", defaultDate );
+  };
+
+  var defaultDays = {
+    min_date: {
+      1: 1, 2: 1, 3: 1, 4:  1,  5: 1,  6: 1,
+      7: 1, 8: 1, 9: 1, 10: 1, 11: 1, 12: 1
+    },
+    max_date: {
+      1: 31, 2: 28, 3: 31, 4:  30,  5: 31,  6: 30,
+      7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31
+    }
+  };
+
+  var defaultDayOfMonth = function( year, month, name ) {
+    return new Date( year, month - 1, defaultDays[name][month] );
   };
 
   return {
