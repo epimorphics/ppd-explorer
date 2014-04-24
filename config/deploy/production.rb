39c3cd120d251bbb3df603cc3b78c78cbc52203e
Ian Dickinson <ian@epimorphics.com>
2014-04-24 15:40:03 +0100

    Updated production deployment script

---
+++ config/deploy/production.rb
@@ -0,0 +1,14 @@
+fetch(:default_env).merge!(rails_env: :production)
+
+set :stage, :production
+set :user, 'ubuntu'
+set :ssh_options, {
+          forward_agent: true
+        }
+
+production_servers = %w(
+  LR-Pres-1
+  LR-Pres-2
+)
+
+role :web, production_servers
