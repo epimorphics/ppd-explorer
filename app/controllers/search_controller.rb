fe1711f7f75c1c3472adb30c31d381c60d546e63
Ian Dickinson <ian@epimorphics.com>
2014-05-01 11:19:02 +0100

    Add error trap to try to diagnose WSOD problem

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,17 +4,29 @@ class SearchController < ApplicationController
   end
 
   def create
-    @preferences = UserPreferences.new( params )
+    begin
+      @preferences = UserPreferences.new( params )
 
-    if @preferences.empty?
-      redirect_to controller: :ppd, action: :index
-    else
-      start = Time.now
-      @query_command = QueryCommand.new( @preferences )
-      @query_command.load_query_results
-      @time_taken = ((Time.now - start) * 1000).to_i
+      if @preferences.empty?
+        redirect_to controller: :ppd, action: :index
+      else
+        start = Time.now
+        @query_command = QueryCommand.new( @preferences )
+        @query_command.load_query_results
+        @time_taken = ((Time.now - start) * 1000).to_i
 
-      render template: "ppd/error" unless @query_command.success?
+        render template: "ppd/error" unless @query_command.success?
+      end
+    rescue => e
+      uuid = SecureRandom.uuid
+
+      Rails.logger.error "Top-level error trap in search_controller #{uuid}"
+      Rails.logger.error "Query error #{uuid} ::: #{e.message}"
+      Rails.logger.error "Query error #{uuid} ::: #{e.backtrace.join("\n")}"
+
+      @error_message = "The log file reference for this error is: #{uuid}."
+      render template: "ppd/error"
+
     end
   end
 end
