972655ffadf2fa9cbdd7aad510f8f4fde0cf8941
Ian Dickinson <ian@epimorphics.com>
2014-11-26 11:59:55 +0000

    Revised fix for APP-01: now apply a smaller truncation limit to just the value of the label, not the entire label

--- app/models/search_term.rb
+++ app/models/search_term.rb
@@ -1,11 +1,11 @@
 class SearchTerm
-  MAX_LABEL_LENGTH = 30
+  MAX_LABEL_TERM_LENGTH = 15
 
   attr_reader :name, :value, :values
 
   def initialize( n, l, v, vs = nil )
     @name = n
-    @label = l
+    @label_prompt, @label_term = l.split( 'matches' )
     @value = v
     @values = vs
   end
@@ -24,18 +24,18 @@ class SearchTerm
   end
 
   def label
-    long_label? ? truncated_label : clean_label
+    "#{@label_prompt} matches #{long_label_term? ? truncated_label_term : clean_label_term}".html_safe
   end
 
-  def long_label?
-    clean_label.length > MAX_LABEL_LENGTH
+  def long_label_term?
+    @label_term.length > MAX_LABEL_TERM_LENGTH
   end
 
-  def truncated_label
-    "#{clean_label.slice( 0, MAX_LABEL_LENGTH )}&hellip;'".html_safe
+  def truncated_label_term
+    "#{clean_label_term.slice( 0, MAX_LABEL_TERM_LENGTH )}&hellip;'".html_safe
   end
 
-  def clean_label
-    @clean_label ||= ProfanityFilter::Base.clean( @label, 'hollow')
+  def clean_label_term
+    @clean_label_term ||= ProfanityFilter::Base.clean( @label_term, 'hollow')
   end
 end
