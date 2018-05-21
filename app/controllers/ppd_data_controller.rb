f6d2b303037ec1bb11d49b1fea570b855058e232
Ian Dickinson <ian.dickinson@epimorphics.com>
2018-05-21 15:51:44 +0100

    Refactor to resolve some Rubocop warnings

--- app/controllers/ppd_data_controller.rb
+++ app/controllers/ppd_data_controller.rb
@@ -7,45 +7,14 @@ class PpdDataController < ApplicationController
 
   rescue_from MalformedSearchError, with: :render_malformed_search_error
 
-  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
   def show
-    @preferences = UserPreferences.new(params)
-    template = choose_template
+    preferences = UserPreferences.new(params)
 
-    if is_explanation?
-      explanation = ExplainCommand.new(@preferences).load_explanation
-      redirect_to "/app/hpi/qonsole?q=#{URI.encode_www_form_component(explanation[:sparql])}"
+    if explanation?
+      show_sparql_explanation(preferences)
     else
-      if is_data_request?
-        @query_command = QueryCommand.new(@preferences)
-        @query_command.load_query_results(limit: :all, download: true, max: MAX_DOWNLOAD_RESULTS)
-
-        template = 'ppd/error' unless @query_command.success?
-      end
-
-      render template
+      prepare_data_download(preferences)
     end
-  end
-  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
-
-  def is_explanation? # rubocop:disable Naming/PredicateName
-    params[:explain]
-  end
-
-  def is_data_request? # rubocop:disable Naming/PredicateName
-    request.format == Mime::Type.lookup_by_extension(:ttl) ||
-      request.format == Mime::Type.lookup_by_extension(:csv)
-  end
-
-  def choose_template
-    template = 'show'
-
-    if @preferences.param('header')
-      template = 'show_with_header'
-      create_download_header
-    end
-
-    template
   end
 
   def render_malformed_search_error(exception = nil)
@@ -58,8 +27,47 @@ class PpdDataController < ApplicationController
 
   private
 
+  def explanation?
+    params[:explain]
+  end
+
+  def data_download?
+    request.format == Mime::Type.lookup_by_extension(:ttl) ||
+      request.format == Mime::Type.lookup_by_extension(:csv)
+  end
+
   def create_download_header
     headers = DownloadRecord::DOWNLOAD_COLUMNS.map { |col| col[:header] }
     @header = headers.join(',')
+  end
+
+  def show_sparql_explanation(preferences)
+    explanation = ExplainCommand.new(preferences).load_explanation
+    redirect_to "/app/hpi/qonsole?q=#{URI.encode_www_form_component(explanation[:sparql])}"
+  end
+
+  def prepare_data_download(preferences)
+    template = choose_template(preferences)
+
+    if data_download?
+      @query_command = QueryCommand.new(preferences)
+      @query_command.load_query_results(limit: :all, download: true, max: MAX_DOWNLOAD_RESULTS)
+
+      template = 'ppd/error' unless @query_command.success?
+    end
+
+    render template
+  end
+
+  def choose_template(preferences)
+    template = 'show'
+
+    if preferences.param('header')
+      template = 'show_with_header'
+      create_download_header
+    end
+
+    @preferences = preferences
+    template
   end
 end
