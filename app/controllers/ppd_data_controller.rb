79ba94fa23866baf3dc6b237be44cb0237c45058
Ian Dickinson <ian.dickinson@epimorphics.com>
2018-05-21 17:35:03 +0100

    Progress on issue #80. Adding support for writing the CSV content to disk and sending the file as data, in the case that the user has requested a CSV file and that file is large.

--- app/controllers/ppd_data_controller.rb
+++ app/controllers/ppd_data_controller.rb
@@ -4,6 +4,7 @@
 class PpdDataController < ApplicationController
   include DsapiTurtleFormatter
   MAX_DOWNLOAD_RESULTS = 1_000_000
+  LARGE_RESULTSET_THRESHOLD = 1
 
   rescue_from MalformedSearchError, with: :render_malformed_search_error
 
@@ -32,8 +33,15 @@ class PpdDataController < ApplicationController
   end
 
   def data_download?
-    request.format == Mime::Type.lookup_by_extension(:ttl) ||
-      request.format == Mime::Type.lookup_by_extension(:csv)
+    csv_data_download? || ttl_data_download?
+  end
+
+  def csv_data_download?
+    request.format == Mime::Type.lookup_by_extension(:csv)
+  end
+
+  def ttl_data_download?
+    request.format == Mime::Type.lookup_by_extension(:ttl)
   end
 
   def create_download_header
@@ -70,15 +78,43 @@ class PpdDataController < ApplicationController
     query_command = QueryCommand.new(preferences)
     query_command.load_query_results(limit: :all, download: true, max: MAX_DOWNLOAD_RESULTS)
 
-    if large_resultset?(query_command)
-      puts 'bang!!!!'
+    if large_csv_resultset?(query_command)
+      render_csv_file(query_command)
+      nil
     else
       @query_command = query_command
       query_command.success? ? template : 'ppd/error'
     end
   end
 
-  def large_resultset?(_query_command)
-    false
+  def large_csv_resultset?(query_command)
+    csv_data_download? && query_command.size > LARGE_RESULTSET_THRESHOLD
+  end
+
+  def render_csv_file(query_command)
+    csv_file = write_csv_file(query_command)
+    send_file(csv_file, filename: 'ppd---data.csv', type: 'text/csv')
+  ensure
+    csv_file&.close
+  end
+
+  def write_csv_file(query_command)
+    file = Tempfile.new(%w[ppd_data csv])
+    Rails.logger.debug "Tempfile is #{file.path}"
+    file.open('w+') do |f|
+      f << @headers if @headers
+      write_csv_rows(query_command, f)
+    end
+
+    file.rewind
+    file
+  end
+
+  def write_csv_rows(query_command, file)
+    query_command.search_results.each_transaction do |sr|
+      row = []
+      DownloadRecord.new(sr).each_column { |csv_value| row << csv_value }
+      file << row.join(',')
+    end
   end
 end
