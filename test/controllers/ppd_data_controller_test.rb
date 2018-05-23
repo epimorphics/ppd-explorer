341c1fa5a7f180bacb1e50b94ec05a7a32b038ca
Ian Dickinson <ian.dickinson@epimorphics.com>
2018-05-23 14:55:35 +0100

    Add a test that ensures that we exercise the file-generation path in CSV downloader. #80

--- test/controllers/ppd_data_controller_test.rb
+++ test/controllers/ppd_data_controller_test.rb
@@ -59,5 +59,24 @@ class PpdDataControllerTest < ActionDispatch::IntegrationTest
         headers.first.must_equal 'unique_id'
       end
     end
+
+    it 'should load a CSV that is large enough to trigger the big-file download behaviour' do
+      VCR.use_cassette('ppd_data_3') do
+        DownloadHelpers.clear_downloads
+        until_december_params = base_params.merge(
+          min_date: '1 January 2014',
+          max_date: '31 December 2017'
+        )
+        visit(ppd_data_path(until_december_params))
+        click_on 'get all results as CSV'
+        sleep 4.seconds
+
+        download_file = File.new(DownloadHelpers.download)
+        csv = CSV.read(download_file)
+        csv.must_be_kind_of Array
+        csv.first.must_be_kind_of Array
+        csv.length.must_be :>, 1000
+      end
+    end
   end
 end
