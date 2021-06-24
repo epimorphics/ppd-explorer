654a4cf079f74899619f0624819e2408afe31a23
bogdan <marc.bogdan96@yahoo.com>
2021-06-24 13:52:33 +0100

    Removed fallback value for API URL for dev and test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,7 +38,7 @@ PpdExplorer::Application.configure do
 
   # API location can be specified in the environment
   # But defaults to the dev service
-  config.api_service_url = ENV['API_SERVICE_URL'] || 'https://lr-ppd-dev-pres.epimorphics.net'
+  config.api_service_url = ENV['API_SERVICE_URL']
 
   config.accessibility_document_path = '/doc/accessibility'
 
