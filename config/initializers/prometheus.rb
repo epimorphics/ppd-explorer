f133744fa4703ae1f22c3cd1d04531fda2222fbd
Ian Dickinson <i.j.dickinson@gmail.com>
2022-01-31 15:09:31 +0000

    Accidentally omitted metrics definitions

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Prometheus counters
 Prometheus::Metrics.register_metric(
   Prometheus::Client::Counter, :data_api_status, 'Response from data API', [:status]
 )
@@ -7,8 +8,13 @@ Prometheus::Metrics.register_metric(
   Prometheus::Client::Counter, :data_api_connection_failure, 'Could not connect to back-end data API'
 )
 Prometheus::Metrics.register_metric(
-  Prometheus::Client::Gauge, :memory_used_mb, 'Process memory usage in mb'
+  Prometheus::Client::Counter, :data_api_service_exception, 'The response from the back-end data API was not processed'
 )
 Prometheus::Metrics.register_metric(
   Prometheus::Client::Counter, :internal_application_error, 'Unexpected events and internal error count'
+)
+
+# Prometheus gauges
+Prometheus::Metrics.register_metric(
+  Prometheus::Client::Gauge, :memory_used_mb, 'Process memory usage in mb'
 )
