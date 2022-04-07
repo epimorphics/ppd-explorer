c09c4dcee7a890236b6d86c0c5f9e18a818fa26f
Ian Dickinson <i.j.dickinson@gmail.com>
2022-04-07 09:26:32 +0100

    Refactor for consistency

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,40 +1,42 @@
 # frozen_string_literal: true
 
+registry = Prometheus::Client.registry
+
 # Prometheus counters
-Prometheus::Client.registry.counter(
+registry.counter(
   :api_status,
   docstring: 'Response from back-end API',
   labels: [:status]
 )
-Prometheus::Client.registry.counter(
+registry.counter(
   :api_requests,
   docstring: 'Overall count of back-end API requests',
   labels: [:result]
 )
-Prometheus::Client.registry.counter(
+registry.counter(
   :api_connection_failure,
   docstring: 'Reasons for back-end API connection failure',
   labels: [:message]
 )
-Prometheus::Client.registry.counter(
+registry.counter(
   :api_service_exception,
   docstring: 'The response from the back-end data API was not processed',
   labels: [:message]
 )
-Prometheus::Client.registry.counter(
+registry.counter(
   :internal_application_error,
   docstring: 'Unexpected events and internal error count',
   labels: [:message]
 )
 
 # Prometheus gauges
-Prometheus::Client.registry.gauge(
+registry.gauge(
   :memory_used_mb,
   docstring: 'Process memory usage in mb'
 )
 
 # Prometheus histograms
-Prometheus::Client.registry.histogram(
+registry.histogram(
   :api_response_times,
   docstring: 'Histogram of response times for API requests',
   buckets: Prometheus::Client::Histogram.exponential_buckets(start: 0.005, count: 16)
