0ad601c02e1cce464b07372e6d0ffcd50e13ee29
der <dave@epimorphics.com>
2022-10-06 16:51:40 +0100

    Disable CSRF to enable load testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+#  protect_from_forgery with: :exception
+  # temporarily disable csrf for load testing
+  protect_from_forgery with: :null_session
 
   before_action :set_phase
   def set_phase
