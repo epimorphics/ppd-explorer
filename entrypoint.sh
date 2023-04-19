9b2e52f2a214fbf57d205179badcc9a63c5f60f7
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2023-04-19 23:42:45 +0100

    build: streamlined entrypoint script
    as per other HMLR apps

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,34 +5,20 @@ set -e
 rm -f ./tmp/pids/server.pid
 mkdir -pm 1777 ./tmp
 
-# This is set to the name of the application for logging purposes
-PUBLIC_NAME="PPD Explorer"
-
 # Set the environment
 [ -z "$RAILS_ENV" ] && RAILS_ENV=production
 
+# Check for API Service URL variable and log it
+[ -n "$API_SERVICE_URL" ] && echo "{\"ts\":\"$(date -u +%FT%T.%3NZ)\",\"level\":\"INFO\",\"message\":\"API_SERVICE_URL=${API_SERVICE_URL}\"}"
 
-echo "{\"ts\": $(date -u +%FT%T.%3NZ), \"level\": \"INFO\", \"message\": \"Initiating ${PUBLIC_NAME} application using APPLICATION_ROOT=${APPLICATION_ROOT}, API_SERVICE_URL=${API_SERVICE_URL}}"
-
-if [ -z "$API_SERVICE_URL" ]
-then
-  echo "{\"ts\": $(date -u +%FT%T.%3NZ), \"level\": \"ERROR\", \"message\": \"You have not specified the env var API_SERVICE_URL\"}" >&2
-  exit 1
-else
-  echo "{\"ts\":\"$(date -u +%FT%T.%3NZ)\",\"level\":\"INFO\",\"message\":\"API_SERVICE_URL=${API_SERVICE_URL}\"}"
-fi
-
+# Check for RAILS_RELATIVE_URL_ROOT variable and log it
+[ -n "$RAILS_RELATIVE_URL_ROOT" ] && echo "{\"ts\":\"$(date -u +%FT%T.%3NZ)\",\"level\":\"INFO\",\"message\":\"RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT}\"}"
 
 # Handle secrets based on env
-if [ "$RAILS_ENV" == "production" ] && [ -z "$SECRET_KEY_BASE" ]
-then
-  SECRET_KEY_BASE=$(./bin/rails secret)
-  export SECRET_KEY_BASE
-fi
+[ "$RAILS_ENV" == "production" ] && [ -z "$SECRET_KEY_BASE" ] && SECRET_KEY_BASE=$(./bin/rails secret) && export SECRET_KEY_BASE
 
-export RAILS_RELATIVE_URL_ROOT=${APPLICATION_ROOT:-'/app/ppd'}
-export SCRIPT_NAME=${RAILS_RELATIVE_URL_ROOT}
+# Log the rails command that will be executed
+echo "{\"ts\":\"$(date -u +%FT%T.%3NZ)\",\"level\":\"INFO\",\"message\":\"exec ./bin/rails server -e ${RAILS_ENV} -b 0.0.0.0\"}"
 
-echo "{\"ts\": $(date -u +%FT%T.%3NZ), \"level\": \"INFO\", \"message\": \"Starting ${PUBLIC_NAME} application with RAILS_ENV=\"${RAILS_ENV}\", \"RAILS_RELATIVE_URL_ROOT\"=\"${RAILS_RELATIVE_URL_ROOT}\", \"SCRIPT_NAME\"=\"${SCRIPT_NAME}\"}"
-
+# Execute the rails command
 exec ./bin/rails server -e "${RAILS_ENV}" -b 0.0.0.0
