d22359f89be4cf3227cdb206828a02da6753a147
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2023-06-14 10:21:27 +0100

    refactor: Removes Sentry from monitoring the test environments

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,7 +4,7 @@ if ENV['SENTRY_API_KEY']
   Sentry.init do |config|
     config.dsn = ENV['SENTRY_API_KEY']
     config.environment = ENV['DEPLOYMENT_ENVIRONMENT'] || Rails.env
-    config.enabled_environments = %w[production test]
+    config.enabled_environments = %w[production]
     config.release = Version::VERSION
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
     config.excluded_exceptions += ['ActionController::BadRequest']
