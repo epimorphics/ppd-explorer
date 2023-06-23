d8e1a3897fed81e88ae242008b0d847a04353995
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2023-06-23 11:13:58 +0100

    chore: additional Rubocop overrides
    as we do want to make sure the parsed message is html safe we suppress the `Rails/OutputSafety` warning for this message.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -47,7 +47,7 @@ class SearchController < ApplicationController
 
   private
 
-  # rubocop:disable Layout/LineLength
+  # rubocop:disable Layout/LineLength, Rails/OutputSafety
   def render_error_page(err, message, status, template = 'ppd/error')
     # link the error to the actual request id otherwise generate one for this error
     uuid = Thread.current[:request_id] || SecureRandom.uuid
@@ -60,5 +60,5 @@ class SearchController < ApplicationController
        '<p>You can quote this reference to support staff so that they can investigate this specific incident.</p>'].join.html_safe
     render(template: template, status: status)
   end
-  # rubocop:enable Layout/LineLength
+  # rubocop:enable Layout/LineLength, Rails/OutputSafety
 end
