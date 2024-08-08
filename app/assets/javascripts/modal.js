62bf9f6e00eaae839fd79bab31c0224dc32e9a74
Daniel Couzens <dan.couzens@epimorphics.com>
2024-08-08 16:39:04 +0100

    updates js syntax to fix build issue

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,51 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function handleModal(modalSelector, focusableElementsSelector, openButtonsSelector, focusOnOpenIndex = 0) {
-      var modal = document.querySelector(modalSelector);
-      var focusableElements = modal.querySelectorAll(focusableElementsSelector);
-      var firstFocusableElement = focusableElements[0];
-      var lastFocusableElement = focusableElements[focusableElements.length - 1];
-      var focusOnOpenElement = focusableElements[focusOnOpenIndex];
-      var openButtons = document.querySelectorAll(openButtonsSelector);
-  
-      function onModalOpen() {
+  function handleModal(
+    modalSelector,
+    focusableElementsSelector,
+    openButtonsSelector,
+    focusOnOpenIndex
+  ) {
+    if (focusOnOpenIndex === undefined) focusOnOpenIndex = 0
+    
+    var modal = document.querySelector(modalSelector)
+    var focusableElements = modal.querySelectorAll(focusableElementsSelector)
+    var firstFocusableElement = focusableElements[0]
+    var lastFocusableElement = focusableElements[focusableElements.length - 1]
+    var focusOnOpenElement = focusableElements[focusOnOpenIndex]
+    var openButtons = document.querySelectorAll(openButtonsSelector)
 
-        if(modalSelector === '#help-dialog') {
-            setTimeout(function () {
-              focusOnOpenElement.focus();
-            }, 10);
-        }
-        
-        document.addEventListener('keydown', function(e) {
-          var isTabPressed = e.key === 'Tab';
-  
-          if (!isTabPressed) {
-            return;
-          }
-  
-          if (e.shiftKey) { 
-            if (document.activeElement === firstFocusableElement) {
-              lastFocusableElement.focus();
-              e.preventDefault();
-            }
-          } else { 
-            if (document.activeElement === lastFocusableElement) { 
-              firstFocusableElement.focus(); 
-              e.preventDefault();
-            }
-          }
-        });
+    function onModalOpen() {
+      if (modalSelector === "#help-dialog") {
+        setTimeout(function () {
+          focusOnOpenElement.focus()
+        }, 10)
       }
-  
-      openButtons.forEach(function(button) {
-        button.addEventListener("click", function (event) {
-          event.preventDefault();
-          onModalOpen();
-        });
-      });
+
+      document.addEventListener("keydown", function (e) {
+        var isTabPressed = e.key === "Tab"
+
+        if (!isTabPressed) {
+          return
+        }
+
+        if (e.shiftKey) {
+          if (document.activeElement === firstFocusableElement) {
+            lastFocusableElement.focus()
+            e.preventDefault()
+          }
+        } else {
+          if (document.activeElement === lastFocusableElement) {
+            firstFocusableElement.focus()
+            e.preventDefault()
+          }
+        }
+      })
     }
-  
-    handleModal('#bookmark-dialog', 'input, .btn, .close', '.action-bookmark');
-  
-    handleModal('#help-dialog', '.close, .trouble-shooting, .email-address, .btn', '.action-help', 1);
-  });
-  
+
+    openButtons.forEach(function (button) {
+      button.addEventListener("click", function (event) {
+        event.preventDefault()
+        onModalOpen()
+      })
+    })
+  }
+
+  handleModal("#bookmark-dialog", "input, .btn, .close", ".action-bookmark")
+
+  handleModal(
+    "#help-dialog",
+    ".close, .trouble-shooting, .email-address, .btn",
+    ".action-help",
+    1
+  )
+})
