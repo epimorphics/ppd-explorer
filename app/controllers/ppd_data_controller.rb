5d811e2d9f3cc23031955463cd97eb225aa2fa21
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-03-05 13:49:47 +0000

    feat: Add logging for initial requests
    - Implemented logging of request parameters and path in two controllers.
    - Enhanced tracking for better debugging and monitoring.

--- app/controllers/ppd_data_controller.rb
+++ app/controllers/ppd_data_controller.rb
@@ -13,6 +13,8 @@ class PpdDataController < ApplicationController
   def show
     preferences = UserPreferences.new(params)
 
+    LoggingHelper.log_request({ params: params, path: request.path }, 'info')
+
     if explanation?
       show_sparql_explanation(preferences)
     else
